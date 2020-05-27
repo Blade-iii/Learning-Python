9dd78e4a30039385b5f83038594a0b704a1c77f4
Blade_iii <hg.swiftnight@gmail.com>
2020-05-27 06:44:33 +0100

    Completed

---
+++ Task 8A.py
@@ -0,0 +1,6 @@
+UserName = input ("What is your Name")
+DisplayName = input ("How many times would you like your name to be displayed")
+
+LoopNum = int(DisplayName)
+for counter in range(0 , LoopNum):
+    print(UserName)
