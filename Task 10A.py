57123a68d3cfb51e44f17659dafc954ff13b62ff
Blade_iii <hg.swiftnight@gmail.com>
2020-05-27 16:38:53 +0100

    Completed

---
+++ Task 10A.py
@@ -0,0 +1,9 @@
+
+UserPass = input ("What is your password")
+UserPass = str(Userpass)
+if Userpass == "Nani":
+    print("Correct Password")
+else:
+    print("Incorrect Password")
+
+#idk how to do the loop
